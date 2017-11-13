db9d5cd8faec0ad6445999db5826cf9df69bbcda
Archomeda <archomeda@hotmail.com>
2017-11-13 04:40:29 +0100

    Change global back to Commando

--- src/data/CommandoPlusSource.js
+++ src/data/CommandoPlusSource.js
@@ -8,7 +8,7 @@ const branchBlacklist = new Set(['gh-pages', 'gh-pages-src', 'docs']);
 export default new DocsSource({
   id: 'commando-plus',
   name: 'Commando-Plus',
-  global: 'Commando-Plus',
+  global: 'Commando',
   repo: 'Archomeda/discord.js-commando-plus',
   defaultTag: 'master',
   branchFilter: branch => !branchBlacklist.has(branch),
